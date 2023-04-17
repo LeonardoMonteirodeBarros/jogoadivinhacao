fc9c6bfeb71842dfd6fbc8aad2b65aef874860cd
Leonardo Monteiro de Barros <123035603+LeonardoMonteirodeBarros@users.noreply.github.com>
2023-04-17 18:31:15 -0300

    refatoraçao

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+// variaveis
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 
 const randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
+
+// Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
 
 // função callback
 function handleTryClick(event) {
@@ -11,8 +18,7 @@ function handleTryClick(event) {
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
+   toggleScreen()
 
     document.querySelector(
       ".screen2 h2"
@@ -22,14 +28,13 @@ function handleTryClick(event) {
   xAttempts++;
 }
 
-// Eventos
-
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1;
-});
+}
+
+function toggleScreen () {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
