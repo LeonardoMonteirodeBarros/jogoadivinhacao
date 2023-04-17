1d3cd20da604f4b23b7ee73f6e4dcf5c0a774f39
Leonardo Monteiro de Barros <123035603+LeonardoMonteirodeBarros@users.noreply.github.com>
2023-04-17 17:52:06 -0300

    correçãobugs

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ function handleTryClick(event) {
     screen1.classList.add("hide");
     screen2.classList.remove("hide");
 
-    document
-    .querySelector(".screen2 h2")
-    .innerText = `Acertou em ${xAttempts} tentativas`;
+    document.querySelector(
+      ".screen2 h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
   }
-
+  inputNumber.value = "";
   xAttempts++;
 }
 
@@ -27,8 +27,8 @@ function handleTryClick(event) {
 const btnTry = document.querySelector("#btnTry");
 const btnReset = document.querySelector("#btnReset");
 
-btnTry.addEventListener('click', handleTryClick);
-btnReset.addEventListener('click', function() {
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", function () {
   screen1.classList.remove("hide");
   screen2.classList.add("hide");
   xAttempts = 1;
